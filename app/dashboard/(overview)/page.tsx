0e0799c5af08de0cc31b8ee8e2644d029d948731
airidwan <a.ridwanudin@outlook.com>
2024-08-18 23:45:50 +0700

    completed chapter 14

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,6 @@ import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
-import {
-  // fetchRevenue,
-  // fetchLatestInvoices,
-  fetchCardData,
-} from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   LatestInvoicesSkeleton,
