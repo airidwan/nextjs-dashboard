233dedec2cfa93663f0dc73850a6153cbfa5e963
airidwan <a.ridwanudin@outlook.com>
2024-08-23 02:38:53 +0700

    completed: Chapter 15 n 16

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,8 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // Untuk test loading skeleton
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
